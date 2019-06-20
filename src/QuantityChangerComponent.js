9de9714dd90f8a3c7a5343acddc27f9b6ffcb862
Andrij <andrey.m.savchuk@gmail.com>
2019-06-20 17:52:37 +0300

    2222

--- src/QuantityChangerComponent.js
+++ src/QuantityChangerComponent.js
@@ -2,25 +2,41 @@ import React from "react";
 import QuantityChanger from "./QuantityChanger";
 
 class QuantityChangerComponent extends React.Component {
-  constructor() {
-    super();
-    this.state = { quantity: 0 };
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: props.product.quantity,
+      id: props.product.id,
+      disabled: false
+    };
     this.decrease = this.decrease.bind(this);
     this.increase = this.increase.bind(this);
   }
 
   decrease() {
+    if (this.state.quantity === 0) {
+      this.setState({ disabled: !this.state.disabled });
+    }
     this.setState({ quantity: this.state.quantity - 1 });
+    console.log(
+      `you have decreased quantity for product with ${this.state.id}`
+    );
   }
 
   increase() {
     this.setState({ quantity: this.state.quantity + 1 });
+    console.log(
+      `you have increased quantity for product with ${this.state.id}`
+    );
   }
 
   render() {
     return (
       <QuantityChanger.Wrapper>
-        <QuantityChanger.DecreaseButton onClick={this.decrease}>
+        <QuantityChanger.DecreaseButton
+          onClick={this.decrease}
+          disabled={this.state.disabled ? "disabled" : ""}
+        >
           -
         </QuantityChanger.DecreaseButton>
         <QuantityChanger.QuantityView>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,28 @@ import QuantityChangerComponent from "./QuantityChangerComponent";
 
 import "./styles.css";
 
+const products = [
+  {
+    quantity: 100,
+    id: 1
+  },
+  {
+    quantity: 150,
+    id: 2
+  },
+  {
+    quantity: 1,
+    id: 3
+  }
+];
+
+const Elements = () => {
+  return products.map(product => (
+    <QuantityChangerComponent product={product} />
+  ));
+};
+
+console.log(Elements);
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<QuantityChangerComponent />, rootElement);
+ReactDOM.render(<Elements />, rootElement);
